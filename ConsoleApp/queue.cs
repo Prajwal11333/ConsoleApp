3f5ab80a0d2e2dfa3f5f7a21fa6e633afd2131f4
PRAJWAL K <cjzf@it.eurofinseu.com>
2026-03-06 23:44:54 +0530

    yy

---
+++ ConsoleApp/queue.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+public class QueueAlgorithms
+{
+    public char FirstNonRepeatingCharacter(string input)
+    {
+        Dictionary<char, int> frequency = new Dictionary<char, int>();
+        Queue<char> queue = new Queue<char>();
+
+        foreach (char c in input)
+        {
+            if (!frequency.ContainsKey(c))
+                frequency[c] = 0;
+
+            frequency[c]++;
+            queue.Enqueue(c);
+
+            while (queue.Count > 0 && frequency[queue.Peek()] > 1)
+            {
+                queue.Dequeue();
+            }
+        }
+
+        return queue.Count > 0 ? queue.Peek() : '#';
+    }
+}
---
+++ TestProject1/UnitTest3.cs
@@ -0,0 +1,56 @@
+﻿using Xunit;
+
+public class QueueAlgorithmsTests
+{
+    [Fact]
+    public void FirstNonRepeatingCharacter_NormalCase()
+    {
+        var algo = new QueueAlgorithms();
+
+        char result = algo.FirstNonRepeatingCharacter("aabbcd");
+
+        Assert.Equal('c', result);
+    }
+
+    [Fact]
+    public void FirstNonRepeatingCharacter_AllRepeating()
+    {
+        var algo = new QueueAlgorithms();
+
+        char result = algo.FirstNonRepeatingCharacter("aabb");
+
+        Assert.Equal('#', result);
+    }
+
+    [Fact]
+    public void FirstNonRepeatingCharacter_FirstCharacterUnique()
+    {
+        var algo = new QueueAlgorithms();
+
+        char result = algo.FirstNonRepeatingCharacter("abcd");
+
+        Assert.Equal('a', result);
+    }
+
+    // Edge case: single character
+    [Fact]
+    public void FirstNonRepeatingCharacter_SingleCharacter()
+    {
+        var algo = new QueueAlgorithms();
+
+        char result = algo.FirstNonRepeatingCharacter("x");
+
+        Assert.Equal('x', result);
+    }
+
+    // Edge case: empty string
+    [Fact]
+    public void FirstNonRepeatingCharacter_EmptyString()
+    {
+        var algo = new QueueAlgorithms();
+
+        char result = algo.FirstNonRepeatingCharacter("");
+
+        Assert.Equal('#', result);
+    }
+}
